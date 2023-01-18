ea47ae4075f580beafd6b10cc0afdb6af5056e6b
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-18 17:42:45 -0300

    feat: Lista de arquivos filtrados

--- progam.js
+++ progam.js
@@ -1,22 +1,37 @@
 /*
 
-  Escreva um programa que usa uma única operação assíncrona de sistema de  
-  arquivos para ler um arquivo e imprimir o número de novas linhas que ele  
-  contém no console (stdout), algo similar à executar cat file | wc -l.  
+  Crie um programa que imprima uma lista de arquivos filtrados por suas  
+  extensões em um dado diretório. Você irá receber um nome de diretório como  
+  primeiro argumento para seu programa (por exemplo:  
+  'caminho/para/diretorio/') e uma extensão de arquivo pela qual filtrar  
+  como segundo argumento.  
    
-  O caminho completo até o arquivo à ser lido será fornecido como primeiro  
-  argumento da linha de comando. 
+  Por exemplo, se você receber 'txt' como segundo argumento, então você  
+  precisará criar um filtro para que a lista contenha apenas arquivos que  
+  terminem com .txt. Note que o segundo argumento não irá vir precedido por  
+  um '.' (ponto).  
+   
+  A lista de arquivos deve ser impressa no console, um arquivo por linha.  
+  Você precisa, obrigatoriamente, usar I/O assíncrono.   
 
 */
-
 const fs = require("fs");
+const { log } = require("console");
+const { extname } = require("path");
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+const dirPath = process.argv[2]
+const fileExtension = process.argv[3]
+
+fs.readdir(dirPath, 'utf8', (err, files) => {
 
   if (err) {
     console.log(err);
   }
 
-  //number of new lines
-  console.log(data.split("\n").length - 1)
+  for (const file of files) {
+    if(extname(file).includes(fileExtension)){
+      log(file)
+    }
+  }
+
 })

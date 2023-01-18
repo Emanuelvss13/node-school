7a74328ea5f278246e527f80e7688a0cc51ec2aa
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-18 17:20:30 -0300

    feat: Meu primeiro I/O

--- progam.js
+++ progam.js
@@ -1,23 +1,22 @@
 /*
 
-  Você vai precisar pensar sobre como fazer uma repetição (loop) através do  
-  número de argumentos para que você possa fornecer apenas a soma deles. O  
-  primeiro elemento do array process.argv é sempre 'node' e o segundo  
-  elemento é sempre o caminho para seu arquivo program.js, então você  
-  precisa começar no terceiro elemento (índice 2), adicionando cada item ao  
-  total até que você atinja o fim do array.  
+  Escreva um programa que usa uma única operação síncrona de sistema de  
+  arquivos (filesystem) para ler e imprimir o número de novas linhas (\n)  
+  que ele contém no console (stdout), algo similar à executar cat file | wc  
+  -l.  
    
-  Também esteja ciente que todos os elementos do process.argv são string e  
-  você pode precisar convertê-los em números. Você pode fazer isso  
-  prefixando a propriedade com + ou passando-os como argumento para  
-  Number(). Por exemplo: +process.argv[2] ou Number(process.argv[2]). 
+  Todo o caminho até o arquivo à ser lido será fornecido como primeiro  
+  argumento da linha de comando.
 
 */
 
-let sum = 0
+const fs = require("fs");
 
-for (let index = 2; index < process.argv.length; index++) {
-  sum += +process.argv[index]
-}
+const file = fs.readFileSync(process.argv[2])
 
-console.log(sum);
+// Passando 'utf8' como segundo argumento no readFileSync ele ja converte o conteúdo do arquivo em string
+fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+
+const numberOfNewLines = file.toString().split("\n").length - 1
+
+console.log(numberOfNewLines);

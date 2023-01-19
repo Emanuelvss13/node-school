223b5b35f172279dede2a42ca7a007049752efa7
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-19 11:24:05 -0300

    feat: Api de servidor de JSON http

--- learn-you-node/progam.js
+++ learn-you-node/progam.js
@@ -11,16 +11,35 @@
 */
 
 const http = require('http');
-const fs = require('fs');
+const url = require("url");
 
 const port = process.argv[2]
 
 const server = http.createServer((req, res) => {
 
-  req
-  .on('data', (chunk) => {
-    res.write(String(chunk).toLocaleUpperCase())
-  })
+  const parsedUrl = url.parse(req.url)
+
+  if(parsedUrl.pathname === "/api/parsetime") {
+    const receivedDate = new Date(parsedUrl.query.split("=")[1])
+
+    const response = {
+      hour: receivedDate.getHours(),
+      minute: receivedDate.getMinutes(),
+      second: receivedDate.getSeconds()
+    }
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(response))
+  }
+
+  if(parsedUrl.pathname === "/api/unixtime"){
+    const receivedDate = new Date(parsedUrl.query.split("=")[1])
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ unixtime: receivedDate.getTime() }))
+  }
+
+  res.writeHead(404)
+  res.end()
 
 })
 

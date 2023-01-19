31864fbd4fca52dd79806d6845526c16b61bc98b
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-18 22:02:31 -0300

    feat: Coletânea http

--- learn-you-node/progam.js
+++ learn-you-node/progam.js
@@ -1,10 +1,13 @@
 /*
 
-  Escreva um programa que realize uma requisição HTTP GET a uma URL  
-  fornecida por você como primeiro argumento na linha de comando. Escreva o  
-  conteúdo da String de cada evento "data" da resposta para uma nova linha  
-  do console (stdout).  
-
+  Escreva um programa que realize uma requisição HTTP GET em uma URL  
+  fornecida por você como o primeiro argumento na linha de comando. Pegue  
+  todos os dados ("data") do servidor (não apenas o primeiro evento "data")  
+  e então escreva duas linhas no console (stdout).  
+   
+  A primeira linha que você vai escrever deve apenas ser um inteiro  
+  representando o número de caracteres recebidos do servidor, e a outra  
+  linha deve conter a String completa de caracteres enviados pelo servidor.
 
 */
 
@@ -12,12 +15,19 @@ var http = require('http');
 
 const url = process.argv[2]
 
+let response = ""
+
 http.get(url, (res) => {
 
   res.setEncoding('utf8')
 
   res.on('data', (data) => {
-    console.log(data);
+      response += data
+  })
+
+  res.on('end', () => {
+    console.log(response.length)
+    console.log(response)
   })
 
 })
